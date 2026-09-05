070f27dbbdee9363823dfda67677eddbda6aaa20
HAZEM <hazem.alkhatib1406@gmail.com>
2026-09-05 18:52:55 +0300

    update pi satus

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-print("hello python")
-print("this is my second change")
-print("Ojhgfd")
-print("JHGF haszem")
-print("this change came from github")
-print("farah")
-name="hazem"
-print(name)
-print("K")
+import time 
+import requests
+from datetime import datetime
+import subprocess
+print("the rasbperry pi is oneline ")
+sleep_time = 3
+for second in range(sleep_time,0,-1):
+    print(second)
+    time.sleep(1)
+result= subprocess.run(
+   ["hostname","-I"],
+   capture_output=True,
+   text=True
+)
+ip = result.stdout.strip()
+ses = subprocess.run(
+    ["hostname"],
+    capture_output=True,
+    text=True 
+)
+hostname = ses.stdout.strip()
+temp_result = subprocess.run(
+    ["vcgencmd","measure_temp"],
+    capture_output=True,
+    text=True
+)
+temperature = temp_result.stdout.strip()
+now = datetime.now()
+time = now.strftime("%H-%M-%S")
+
+current = datetime.now()
+date = now.strftime("%Y:%m:%d")
+
+url = "https://ntfy.sh/gh-pi"
+message = f""" IP : {ip},
+               HOSTNAME : {hostname},
+               TIME : {time},
+               DATE : {date},
+               TEMPERATURE :{temperature}
+"""
+requests.post(url,data=message)
+
